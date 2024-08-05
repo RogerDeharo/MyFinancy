142547687c4a81fd9dcf4e2efca99b70ebee1e66
RogerDeharo <roger.deharo@hotmail.com>
2024-08-05 15:16:08 -0300

    first commit

---
+++ lib/common/constants/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  AppColors();
+
+  static const Color greelightOne = Color(0xFF191A32);
+  static const Color greelightTwo = Color(0xFF191A32);
+  static const List<Color> greenGradrient = [Color(0xFF323755), Color(0xFF323755)];
+  static const Color white = Color(0xFFFFFFFF);
+}
